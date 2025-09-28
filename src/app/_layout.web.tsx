25ef1afaeb4a4df911d499295ae56152e5309110
Frank Calise <fcalise@gmail.com>
2025-09-28 10:50:33 -0400

    fix(web): add remaining fonts

--- src/app/_layout.web.tsx
+++ src/app/_layout.web.tsx
@@ -12,6 +12,8 @@ export default function RootLayout() {
   const [loaded] = useFonts({
     "Oxanium-Regular": require("../../assets/fonts/Oxanium-Regular.ttf"),
     "Oxanium-Bold": require("../../assets/fonts/Oxanium-Bold.ttf"),
+    "Oxanium-Medium": require("../../assets/fonts/Oxanium-Medium.ttf"),
+    "Oxanium-SemiBold": require("../../assets/fonts/Oxanium-SemiBold.ttf"),
   })
   if (!loaded) {
     return null
