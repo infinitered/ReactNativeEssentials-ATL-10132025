a03d68c082ce98ff9c606423da76d0cd625b03cb
Daniel Edwards <cdaniel.edwards@gmail.com>
2025-09-29 10:41:41 -0400

    feat: integrate Mock Service Worker (MSW) to debugging structure and enhance development tools
    - Added MSW for API mocking with new handlers for game data.
    - Created a unified setup for development tools, including Reactotron and MSW.
    - Updated layout files to utilize the new development tools setup.
    - Removed obsolete MSW index file and adjusted postinstall script for Expo.
    - Enhanced utility functions for game data preparation and camel case conversion.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,9 @@
 import { Stack } from "expo-router"
 
+import { setupDevtools } from "../devtools/setup"
+
 if (__DEV__) {
-  // Load Reactotron configuration in development. We don't want to
-  // include this in our production bundle, so we are using `if (__DEV__)`
-  // to only execute this in development.
-  require("src/devtools/ReactotronConfig.ts")
-  
-  // Setup MSW for API mocking in development
-  const { setupMockServer } = require("../../msw/index.ts")
-  setupMockServer({ debug: true })
+  setupDevtools()
 }
 
 export default function RootLayout() {
