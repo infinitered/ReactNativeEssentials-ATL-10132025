2f18ac18ae01e19404cfc572c5e599749bab4bfc
Frank Calise <fcalise@gmail.com>
2025-10-08 17:48:20 -0400

    fix(boilerplate): theme import

--- src/screens/GamesListScreen.tsx
+++ src/screens/GamesListScreen.tsx
@@ -2,7 +2,7 @@ import type { TextStyle, ViewStyle } from "react-native"
 import { View } from "react-native"
 
 import { Text } from "@components/Text"
-import { colors, sizes } from "@shared/theme"
+import { colors, sizes } from "@theme/index"
 
 export const GamesListScreen = () => {
   return (
