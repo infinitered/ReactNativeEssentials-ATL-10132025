f6bd748ea2d528e8523b14cfb32a934afb584325
Frank Calise <fcalise@gmail.com>
2025-10-08 17:56:16 -0400

    fix(solutions): update with theme files

---
+++ solutions/chapter3/theme/tokens/darkColorTextSemantics.ts
@@ -0,0 +1,10 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 12 Jan 2024 04:20:14 GMT
+ */
+
+export const base = "#ffffffff"
+export const accent = "#f8baf7ff"
+export const brand = "#d4f20dff"
+export const baseMuted = "#bbbbbbff"
+export const overlay = "#ffffffff"
