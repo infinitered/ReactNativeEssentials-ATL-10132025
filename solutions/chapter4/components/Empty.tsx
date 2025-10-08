f776785636d90636849df2ff353bec7d5239659e
Frank Calise <fcalise@gmail.com>
2025-10-08 10:45:06 -0400

    fix(solutions): ch4 wip

---
+++ solutions/chapter4/components/Empty.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import type { TextStyle, ViewStyle } from "react-native"
+import { View } from "react-native"
+
+import { colors, sizes } from "@shared/theme"
+
+import type { IconProps } from "./Icon"
+import { Icon } from "./Icon"
+import { Text } from "./Text"
+
+export const Empty = (props: { text?: string; icon?: IconProps["name"] }) => {
+  return (
+    <View style={$emptyContentWrapper}>
+      <Icon color={colors.tint.baseMuted} size={36} name={props.icon ?? "frown"} />
+      <Text preset="display" text={props.text ?? "There's\nNothing Here..."} style={$emptyText} />
+    </View>
+  )
+}
+
+const $emptyContentWrapper: ViewStyle = {
+  flexDirection: "row",
+  paddingVertical: sizes.spacing.xl,
+}
+
+const $emptyText: TextStyle = {
+  color: colors.text.baseMuted,
+  marginStart: sizes.spacing.md,
+}
--- solutions/chapter4/screens/GamesListScreen.tsx
+++ solutions/chapter4/screens/GamesListScreen.tsx
@@ -1,11 +1,59 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useCallback, useEffect } from "react"
+import type { ViewStyle } from "react-native"
+import { FlatList, Pressable } from "react-native"
+import { Link, useRouter } from "expo-router"
+import { Card } from "@components/Card"
+import { Empty } from "@components/Empty"
+import { useGlobalState } from "@services/state"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
+
+import { api } from "@shared/services/api"
+import { colors, sizes } from "@shared/theme"
 
 export const GamesListScreen = () => {
+  const { bottom: paddingBottom } = useSafeAreaInsets()
+  const router = useRouter()
+  const { games, setGames } = useGlobalState()
+
+  const getGames = useCallback(async () => {
+    const response = await api.getGames()
+    if (response.ok) {
+      setGames(response.data)
+    }
+  }, [setGames])
+
+  useEffect(() => {
+    getGames()
+  }, [getGames])
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Chapter 4: Blending In - Coming Soon</Text>
-      <Text>Games List Screen with Theming</Text>
-    </View>
+    <FlatList
+      data={games}
+      style={$list}
+      keyExtractor={(item) => String(item.id)}
+      contentContainerStyle={[{ paddingBottom }, $contentContainer]}
+      ListEmptyComponent={<Empty />}
+      renderItem={({ item }) => (
+        <Link asChild href={`/games/${item.id}`} key={item.id}>
+          <Pressable>
+            <Card
+              name={item.name}
+              rating={item.totalRatingStars}
+              releaseDate={item.releaseDate.human}
+              imageUrl={item.cover.imageUrl}
+            />
+          </Pressable>
+        </Link>
+      )}
+    />
   )
+}
+
+const $list: ViewStyle = {
+  backgroundColor: colors.background.primary,
+}
+
+const $contentContainer: ViewStyle = {
+  rowGap: sizes.spacing.lg,
+  padding: sizes.spacing.md,
 }
