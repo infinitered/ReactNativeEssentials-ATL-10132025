42d347375285fbebedc715ff853cadfabefe38f4
Frank Calise <fcalise@gmail.com>
2025-10-09 21:33:01 -0400

    fix(ch3): add section list to solution

--- solutions/chapter3/components/Pill.tsx
+++ solutions/chapter3/components/Pill.tsx
@@ -1,1 +1,34 @@
-export const Pill = () => null
+import type { TextStyle, ViewStyle } from "react-native"
+import { View } from "react-native"
+
+import { colors, sizes } from "@theme/index"
+
+import { Text } from "./Text"
+
+interface PillProps {
+  text: string
+}
+
+export const Pill = (props: PillProps) => {
+  return (
+    <View style={$pill}>
+      <Text preset="label1" text={props.text} style={$text} />
+    </View>
+  )
+}
+
+const $pill: ViewStyle = {
+  alignItems: "center",
+  alignSelf: "flex-start",
+  backgroundColor: colors.background.accent,
+  borderColor: colors.border.base,
+  borderRadius: sizes.radius.md,
+  borderWidth: sizes.border.sm,
+  height: sizes.spacing.xl,
+  justifyContent: "center",
+  paddingHorizontal: sizes.spacing.md,
+}
+
+const $text: TextStyle = {
+  color: colors.text.brand,
+}
